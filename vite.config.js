6e32b77d9b4b047baa1e4c63e5dfc1f42fd2fac4
M.A.A Shaker <120217623+MichaelShaker@users.noreply.github.com>
2026-04-18 14:13:24 +0200

    add github pages workflow

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/FormatBible/',1
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
